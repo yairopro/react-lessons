8f9f68cd58508efc510a5edeefa0d053f99f7a3d
Yair <yairopro@gmail.com>
2022-11-08 14:24:08 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import Text from "./component/Text"
+
+export default function App() {
+	return (
+		<View>
+			<Text>
+				{"Hello BA-LINK"}
+			</Text>
+		</View>
+	);
+}
